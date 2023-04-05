53c1c0a580c5786f12b85fb33ddd979cfd9948fd
Oleksandr Korotkyi <oleksandr.korotkyi@oracle.com>
2023-04-05 15:26:39 +0300

    book detail page added

---
+++ Website/Website/Controllers/BookController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Website.Infrastructure.ModelBinding;
+using Website.Infrastructure.Repositories;
+
+namespace Website.Controllers
+{
+    public class BookController : Controller
+    {
+        IBooksRepository booksRepository;
+        IUsersRepository usersRepository;
+        IRecommendationsRepository recommendationsRepository;
+
+        public BookController(IBooksRepository booksRepo, IUsersRepository usersRepo, IRecommendationsRepository recoRepo)
+        {
+            booksRepository = booksRepo;
+            usersRepository = usersRepo;
+            recommendationsRepository = recoRepo;
+        }
+
+        [Route("[controller]/{id}")]
+        public IActionResult Index([FromCookie] Guid? UserId, int id)
+        {
+            long? lngUserId = null;
+
+            if (UserId != null)
+            {
+                var user = usersRepository.GetByUniqueId(UserId.Value);
+                if (user != null)
+                {
+                    lngUserId = user.Id;
+                }
+            }
+
+            ViewData["Recommendations"] = recommendationsRepository.GetForUser(lngUserId);
+
+            var book = booksRepository.Get(id);
+            return View(book);
+        }
+    }
+}
