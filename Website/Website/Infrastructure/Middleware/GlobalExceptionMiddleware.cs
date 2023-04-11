229047c7fdc3279015a122f4e635fe5fd7ccf4f5
Oleksandr Korotkyi <oleksandr.korotkyi@oracle.com>
2023-04-11 22:25:25 +0300

    centralized error logging implemented

---
+++ Website/Website/Infrastructure/Middleware/GlobalExceptionMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Website.Infrastructure.Middleware
+{
+    public class GlobalExceptionMiddleware
+    {
+        private readonly RequestDelegate next;
+        public readonly Serilog.ILogger log;
+
+        public GlobalExceptionMiddleware(RequestDelegate _next)
+        {
+            next = _next;
+            log = Log.Logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+
+                switch (ex)
+                {
+
+                    default:
+                        // unhandled error
+                        log.Error(" GlobalException:" + ex.ToString());
+                        break;
+                }
+
+                throw;
+            }
+        }
+    }
+}
