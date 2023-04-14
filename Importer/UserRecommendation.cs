fec4781bf8af8fa6f90188405b68fc8420f75430
Oleksandr Korotkyi <oleksandr.korotkyi@oracle.com>
2023-04-14 21:56:15 +0300

    Importer implemented, database backups with new procedures updated

---
+++ Importer/UserRecommendation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Importer
+{
+    public class UserRecommendation
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public long ItemId { get; set; }
+
+    }
+}
