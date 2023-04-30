65a13d33a110043b5b7b8f23a5b5c4b6d8bdd88b
Oleksandr Korotkyi <oleksandr.korotkyi@oracle.com>
2023-04-30 14:07:14 +0300

    My favorites page added

---
+++ Website/Website/Controllers/MyFavoritesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Website.Infrastructure.ModelBinding;
+using Website.Infrastructure.Repositories;
+using Website.Models.Converters;
+using Website.Models.Requests;
+using Website.Models.Web;
+
+namespace Website.Controllers
+{
+    public class MyFavoritesController : BaseUIController
+    {
+        IBooksRepository booksRepository;
+        IUsersRepository usersRepository;
+        IRecommendationsRepository recommendationsRepository;
+
+        public MyFavoritesController(ILogger<HomeController> logger, IBooksRepository booksRepo, IUsersRepository usersRepo, IRecommendationsRepository recoRepo) : base(logger)
+        {
+            booksRepository = booksRepo;
+            usersRepository = usersRepo;
+            recommendationsRepository = recoRepo;
+        }
+
+        public IActionResult Index([FromCookie] Guid? UserId, BookUISearchRequest request)
+        {
+            request.RatedOnly = 1;
+
+            var searchRequest = BookSearchConverter.Convert(request);
+
+            long? lngUserId = null;
+
+            if (UserId != null)
+            {
+                var user = usersRepository.GetByUniqueId(UserId.Value);
+                if (user != null)
+                {
+                    lngUserId = user.Id;
+                    searchRequest.UserId = user.Id;
+                }
+            }
+
+            ViewData["Recommendations"] = recommendationsRepository.GetForUser(lngUserId);
+
+            var list = booksRepository.Search(searchRequest);
+            return View(new BooksSearchAndResult() { Request = request, Result = list });
+
+        }
+    }
+}
--- Website/Website/wwwroot/js/like-page.js
+++ Website/Website/wwwroot/js/like-page.js
@@ -21,6 +21,19 @@ function showicon () {
 /* =========================like-star--rating============================= */
 
 let star = document.querySelectorAll('input');
+
+for (let i = 0; i < star.length; i++) {
+    star[i].addEventListener('click', function () {
+        r = event.target.value;
+        itemId = event.target.parentElement.id.replace("rating_", "");
+        setRating(itemId, r);
+        let showValue = document.querySelector('#rating-value_' + itemId.toString());
+        showValue.innerHTML = r + " out of 5"
+    })
+}
+
+/*
+let star = document.querySelectorAll('input');
 let showValue = document.querySelector('#rating-value');
 
 for (let i = 0; i < star.length; i++){
@@ -30,6 +43,49 @@ for (let i = 0; i < star.length; i++){
     showValue.innerHTML = i + " out of 5"
   })
 }
+
+
+let star2 = document.querySelectorAll('input');
+let showValue2 = document.querySelector('#rating-value2');
+
+for (let i = 0; i < star2.length; i++){
+  star2[i].addEventListener('click', function(){
+    i = this.value;
+
+    showValue2.innerHTML = i + " out of 5"
+  })
+}
+let star3 = document.querySelectorAll('input');
+let showValue3 = document.querySelector('#rating-value3');
+
+for (let i = 0; i < star3.length; i++){
+  star3[i].addEventListener('click', function(){
+    i = this.value;
+
+    showValue3.innerHTML = i + " out of 5"
+  })
+}
+let star4 = document.querySelectorAll('input');
+let showValue4 = document.querySelector('#rating-value4');
+
+for (let i = 0; i < star4.length; i++){
+  star4[i].addEventListener('click', function(){
+    i = this.value;
+
+    showValue4.innerHTML = i + " out of 5"
+  })
+}
+let star5 = document.querySelectorAll('input');
+let showValue5 = document.querySelector('#rating-value5');
+
+for (let i = 0; i < star5.length; i++){
+  star5[i].addEventListener('click', function(){
+    i = this.value;
+
+    showValue5.innerHTML = i + " out of 5"
+  })
+}
+*/
 
 /* =========================like-slider============================= */
 
@@ -54,5 +110,3 @@ $(function(){
   })
   
   })
-
-  
